927075623796296b648f9d2a51b0fe789df8e546
MohamedShakshak <108498735+MohamedShakshak@users.noreply.github.com>
2025-01-08 22:01:43 +0200

    Update QA.py

--- QA.py
+++ QA.py
@@ -3,6 +3,7 @@ import numpy as np
 import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.applications.inception_v3 import preprocess_input
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from PIL import Image
