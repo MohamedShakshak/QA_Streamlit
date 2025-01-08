e4be4020b76d677ea852324c115b6fbbb7533292
MohamedShakshak <108498735+MohamedShakshak@users.noreply.github.com>
2025-01-08 22:22:37 +0200

    Update QA.py

--- QA.py
+++ QA.py
@@ -5,6 +5,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.applications.inception_v3 import preprocess_input
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing import sequence
 from PIL import Image
 import requests
 import matplotlib.pyplot as plt
@@ -51,6 +52,43 @@ def greedySearch(photo):
             break
     return ' '.join(in_text.split()[1:-1])
 
+def beam_search_predictions(image, beam_index = 3):
+    start = [wordtoix["startseq"]]
+    start_word = [[start, 0.0]]
+    while len(start_word[0][0]) < max_length:
+        temp = []
+        for s in start_word:
+            par_caps = sequence.pad_sequences([s[0]], maxlen=max_length, padding='post')
+            preds = caption_model.predict([image,par_caps], verbose=0)
+            word_preds = np.argsort(preds[0])[-beam_index:]
+            # Getting the top <beam_index>(n) predictions and creating a 
+            # new list so as to put them via the model again
+            for w in word_preds:
+                next_cap, prob = s[0][:], s[1]
+                next_cap.append(w)
+                prob += preds[0][w]
+                temp.append([next_cap, prob])
+                    
+        start_word = temp
+        # Sorting according to the probabilities
+        start_word = sorted(start_word, reverse=False, key=lambda l: l[1])
+        # Getting the top words
+        start_word = start_word[-beam_index:]
+    
+    start_word = start_word[-1][0]
+    intermediate_caption = [ixtoword[i] for i in start_word]
+    final_caption = []
+    
+    for i in intermediate_caption:
+        if i != 'endseq':
+            final_caption.append(i)
+        else:
+            break
+
+    final_caption = ' '.join(final_caption[1:])
+    return final_caption
+
+
 # Streamlit App
 st.title("Image Captioning App")
 st.write("Upload an image or provide a URL to generate captions.")
@@ -85,6 +123,9 @@ if image:
             greedy_caption = greedySearch(encoded_image)
         st.success("Captioning Completed!")
         st.write(f"**Greedy Search Caption:** {greedy_caption}")
+        st.write(f"**Beam Search, K = 3:** {beam_search_predictions(encoded_image, beam_index = 3)}")
+        st.write(f"**Beam Search, K = 5:** {beam_search_predictions(encoded_image, beam_index = 5)}")
+        st.write(f"**Beam Search, K = 7:** {beam_search_predictions(encoded_image, beam_index = 7)}")
 else:
     st.info("Please upload an image or provide a valid URL to proceed.")
 
