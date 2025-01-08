642ea9b7695e877cff7b21517973813fe05ce4b8
MohamedShakshak <108498735+MohamedShakshak@users.noreply.github.com>
2025-01-08 22:08:39 +0200

    Update QA.py

--- QA.py
+++ QA.py
@@ -58,7 +58,7 @@ def beam_search_predictions(image, beam_index = 3):
         temp = []
         for s in start_word:
             par_caps = sequence.pad_sequences([s[0]], maxlen=max_length, padding='post')
-            preds = model.predict([image,par_caps], verbose=0)
+            preds = caption_model.predict([image,par_caps], verbose=0)
             word_preds = np.argsort(preds[0])[-beam_index:]
             # Getting the top <beam_index>(n) predictions and creating a 
             # new list so as to put them via the model again
